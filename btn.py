970467c35c77fb0770a733ac68bd403e94a6e121
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-11-10 20:12:32 +0330

    use attributes button and comenting in project

---
+++ btn.py
@@ -0,0 +1,35 @@
+from customtkinter import *
+
+
+
+window = CTk()
+
+window.title('amir')
+
+window.geometry('500x400')
+
+def click():
+    print('گی')
+
+btn = CTkButton(
+    window,
+    #width=45, # اندازه 
+    #height=10, # اندازه ارتفاع
+    text='Click me', # تکست داخل دکمه
+    corner_radius=50, # گرد کردن حاشیه های هر چیزی
+    border_width=3, # اندازه بردر دور هر چیزی
+    border_color='black', # رنگ بردر دور
+    #border_spacing=6, # فاصله داخلی بردر
+    fg_color='red', # رنگ هم با اسم هم با کد کالر
+    hover_color='pink', # رنگ هاور
+    text_color= 'black', # رنگ تکست
+    #image= '', # آوردن عکس در هرجا
+    #state='disable', # از کار انداختن 
+    #hover=False, # استفاده از هاور آره یا نه
+    command=click, # دادن دستور
+    
+)
+
+btn.pack()
+
+window.mainloop()
--- new.py
+++ new.py
@@ -2,21 +2,21 @@ from customtkinter import *
 
 window = CTk()
 
-width = window.winfo_screenwidth()
-height = window.winfo_screenheight()
+width = window.winfo_screenwidth() # گرفتن ساز مانیتور
+height = window.winfo_screenheight() # گرفتن ساز مانیتور
 
 btn = CTkButton(window,text='click me',command=window.destroy)
 btn.pack()
 
-window.geometry(f'{width}x{height}+0+0')
-# window.geometry(f'{width}x{height}')
+window.geometry(f'{width}x{height}+0+0') # فول اسکرن کردن با این ها صورت میگیرد
+# window.geometry(f'{width}x{height}') # فول اسکرن کردن با این ها صورت میگیرد
 
 
-# window.after(0,lambda :window.state('zoomed'))
+# window.after(0,lambda :window.state('zoomed')) # فول اسکرن کردن با این ها صورت میگیرد
 
 # window.wm_attributes('-fullscreen', True)
 
-# window.state('zoomed')
+# window.state('zoomed') # فول اسکرن کردن با این ها صورت میگیرد
 
 window._set_appearance_mode("system")
 
