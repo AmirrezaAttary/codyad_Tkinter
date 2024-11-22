64c71021f1733ae667ab4ed95ebc1ad044f1ab05
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-11-22 19:27:13 +0330

    takmil page paziresh

---
+++ test.py
@@ -0,0 +1,45 @@
+# from persiantools.jdatetime import JalaliDate
+# import datetime
+
+# print(JalaliDate.today())
+
+# miladi = JalaliDate(1396,7,1).to_gregorian()
+
+# print(miladi)
+
+import sqlite3
+data_person_name = [('amirrr','attary')]
+
+con = sqlite3.connect("mydb.db")
+
+
+
+c = con.cursor()
+
+c.execute('''create table if not exists q1_person_name
+                 (name_id INTEGER PRIMARY KEY,
+                  first_name varchar(20) NOT NULL,
+                  last_name varchar(20) NOT NULL)''')
+c.executemany('INSERT INTO q1_person_name(first_name, last_name) VALUES (?,?)', data_person_name)
+
+for row in c.execute('SELECT * FROM q1_person_name'):
+    a = row
+b = a[0]
+b=int(b)
+print(b)
+con.commit()
+con.close()
+
+# import sqlite3
+
+# conn = sqlite3.connect('memo.db')
+# cursor = conn.cursor()
+
+# a=cursor.execute("SELECT name_id FROM q1_person_name ORDER BY name_id DESC LIMIT 1")
+# last_id = cursor.lastrowid
+# print(a)
+
+# conn.commit()
+# cursor.close()
+# conn.close()
+
