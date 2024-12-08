e3616d430a42374ba5242e8a4fca10d23c3557e4
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-12-08 10:18:19 +0330

    end tarikh

--- project.py
+++ project.py
@@ -53,6 +53,21 @@ def validate_phone(phone_number):
   pattern = r"^\+?\d{11}$" #Example: Accepts numbers starting with optional +, min 10 digits
   return bool(re.match(pattern, phone_number))
 
+def update_data(id_,data):
+    print(id_)
+    print(data)
+    try:
+        conn = sqlite3.connect('pz.db')
+        cursor = conn.cursor()
+        cursor.execute(f"UPDATE paziresh SET (arrival, departure, system, system_type, system_model, system_serial, input_name, tel, code_meli, tel_sabet, adress, problem, cost, description, Warranty, Warranty_modat, Technician_opinion, status) = ({data}) WHERE id={id_}")
+        conn.commit()
+        conn.close()
+        return True
+    except sqlite3.Error as e:
+        print(f"Database error: {e}") # More informative error message
+        return False
+
+
 def validate_code_meli(code_meli):
   #Basic code meli validation (Adjust if needed)
   pattern = r"^\d{10}$" # Example: 10 digits
@@ -1117,6 +1132,134 @@ def tarikh_():
 
 ###########################################################################################
 def show(data):
+    clear_frame()
+    try:
+        conn = sqlite3.connect('pz.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT MAX(id) FROM paziresh")
+        result = cursor.fetchone()
+        b = result[0] + 1 if result[0] is not None else 1
+        b+=1590
+        conn.close()
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred while getting the next ID: {e}")
+    
+    
+    def save_data():
+        
+        try:
+            arrival = ent_date_of_arrival.get()
+            departure = ent_departure_date.get()
+            system = ent_input_system.get()
+            system_type = combo_input_system_type.get()
+            system_model = ent_input_system_model.get()
+            try:
+                system_serial = int(ent_input_system_serial.get())
+            except ValueError:
+                return messagebox.showerror("Error", "Serial number must be an integer.")
+            input_name = ent_input_name.get()
+            tel = ent_input_name_tel.get()
+            code_meli = ent_input_name_code_meli.get()
+            tel_sabet = ent_input_name_tel_sabet.get()
+            adress = ent_input_name_adress.get("1.0", END).strip() #Get text from textbox
+            problem = ent_problem.get("1.0", END).strip()
+            try:
+                cost = float(ent_cost.get())
+            except ValueError:
+                return messagebox.showerror("Error", "Cost must be a number.")
+            description = ent_input_description.get("1.0", END).strip() #Get text from textbox
+            Warrantyy = chek.get() 
+            Warrantyy_mod = ent_modat_garanti.get()
+            op_tek = ent_tak.get(0.0,END)
+            status = chek_exit.get()
+            if not all([system, system_type, system_model, input_name, tel, code_meli]):
+                return messagebox.showerror("Error", "Please fill in all required fields.")
+            if not validate_phone(tel):
+                return messagebox.showerror("Error","Invalid phone number.")
+            if not validate_code_meli(code_meli):
+                return messagebox.showerror("Error","Invalid code meli.")
+
+            data_tuple = (arrival, departure, system, system_type, system_model, system_serial, input_name, tel, code_meli, tel_sabet, adress, problem, cost, description,Warrantyy,Warrantyy_mod,op_tek,status)
+            result = update_data(data,data_tuple)
+            if isinstance(result, bool) and result: #Check if insert_data returned True(success)
+                messagebox.showinfo("Success", "Data saved successfully!")
+            else:
+                messagebox.showerror("Error", result) #Show specific error message
+        except Exception as e:
+            messagebox.showerror("Error", f"An unexpected error occurred: {e}")
+        dashbord()
+        
+    def save_data_print():
+        
+        try:
+            arrival = ent_date_of_arrival.get()
+            departure = ent_departure_date.get()
+            system = ent_input_system.get()
+            system_type = combo_input_system_type.get()
+            system_model = ent_input_system_model.get()
+            try:
+                system_serial = int(ent_input_system_serial.get())
+            except ValueError:
+                return messagebox.showerror("Error", "Serial number must be an integer.")
+            input_name = ent_input_name.get()
+            tel = ent_input_name_tel.get()
+            code_meli = ent_input_name_code_meli.get()
+            tel_sabet = ent_input_name_tel_sabet.get()
+            adress = ent_input_name_adress.get("1.0", END).strip() #Get text from textbox
+            problem = ent_problem.get("1.0", END).strip()
+            try:
+                cost = float(ent_cost.get())
+            except ValueError:
+                return messagebox.showerror("Error", "Cost must be a number.")
+            description = ent_input_description.get("1.0", END).strip() #Get text from textbox
+            Warrantyy = chek.get() 
+            Warrantyy_mod = ent_modat_garanti.get()
+            op_tek = ent_tak.get(0.0,END)
+            status = chek_exit.get()
+            if not all([system, system_type, system_model, input_name, tel, code_meli]):
+                return messagebox.showerror("Error", "Please fill in all required fields.")
+            if not validate_phone(tel):
+                return messagebox.showerror("Error","Invalid phone number.")
+            if not validate_code_meli(code_meli):
+                return messagebox.showerror("Error","Invalid code meli.")
+
+            data_tuple = (arrival, departure, system, system_type, system_model, system_serial, input_name, tel, code_meli, tel_sabet, adress, problem, cost, description,Warrantyy,Warrantyy_mod,op_tek,status)
+            result = update_data(data,data_tuple)
+            if isinstance(result, bool) and result: #Check if insert_data returned True(success)
+                messagebox.showinfo("Success", "Data saved successfully!")
+                curr_time = time.strftime("%H:%M:%S", time.localtime())
+                context = {
+                    'id':ent_input_id.get(),
+                'garanti':chek.get(),
+                'tarikh':ent_date_of_arrival.get(),
+                'date':curr_time,
+                'name_input':ent_input_name.get(),
+                'system_name':ent_input_system.get(),
+                'typee':combo_input_system_type.get(),
+                'model':ent_input_system_model.get(),
+                'seriyal':ent_input_system_serial.get(),
+                'tel':ent_input_name_tel.get(),
+                'addres':ent_input_name_adress.get("1.0", END),
+                'moshkel':ent_input_description.get("1.0", END),
+                'modat' : ent_modat_garanti.get()
+                }
+                temp_loder = jinja2.FileSystemLoader('./')
+                temp_env = jinja2.Environment(loader=temp_loder)
+                temp = temp_env.get_template('atar.html')
+                output_text = temp.render(context)
+                # Replace with your path
+                config = pdfkit.configuration(wkhtmltopdf = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+                pdfkit.from_string(output_text,f'./ex/sefaresh_{ent_input_id.get()}_mojadad.pdf',options={"encoding":'UTF-8','page-width': 210,'page-height': 151},configuration=config,)
+                messagebox.showinfo("چاپ",'رسید پرینت شد')
+            else:
+                messagebox.showerror("Error", result) #Show specific error message
+        except Exception as e:
+            messagebox.showerror("Error", f"An unexpected error occurred: {e}")
+        dashbord()
+        
+    
+    
+    
     con = sqlite3.connect("pz.db")
     c = con.cursor()
     for i in c.execute(f'SELECT * FROM paziresh WHERE id={data}'):
@@ -1128,10 +1271,10 @@ def show(data):
         bt_back = customtkinter.CTkButton(f_show, text="بازگشت",font=font, fg_color= '#EA0000', hover_color = '#B20000', command=dashbord)
         bt_back.grid(row=13, column=0, padx=20, pady=(10, 0),sticky=E)
 
-        bt_save = customtkinter.CTkButton(f_show, text="ذخیره",font=font,fg_color="green",hover_color="#00ff00",)
+        bt_save = customtkinter.CTkButton(f_show, text="ذخیره",font=font,fg_color="green",hover_color="#00ff00",command=save_data)
         bt_save.grid(row=13, column=1, padx=20, pady=(10, 0),)
         
-        bt_save = customtkinter.CTkButton(f_show, text="ذخیره و پرینت",font=font,fg_color="green",hover_color="#00ff00",)
+        bt_save = customtkinter.CTkButton(f_show, text="ذخیره و پرینت",font=font,fg_color="green",hover_color="#00ff00",command=save_data_print)
         bt_save.grid(row=13, column=2, padx=20, pady=(10, 0),sticky=W)
         
         lbl_id = CTkLabel(f_show, font=font,text=": شماره پذریش ")
@@ -1195,12 +1338,12 @@ def show(data):
         lbl_input_name_tel = CTkLabel(frame_down,text=" : شماره تلفن",font=font)
         lbl_input_name_tel.grid(row=0,column=3)
         ent_input_name_tel = CTkEntry(frame_down,font=font_enry,justify=LEFT)
-        ent_input_name_tel.insert(0,i[8])
+        ent_input_name_tel.insert(0,f"0{i[8]}")
         ent_input_name_tel.grid(row=0,column=2)
         lbl_input_name_code_meli = CTkLabel(frame_down,text=" : کد ملی ",font=font)
         lbl_input_name_code_meli.grid(row=0,column=1)
         ent_input_name_code_meli = CTkEntry(frame_down,font=font_enry,justify=LEFT)
-        ent_input_name_code_meli.insert(0,i[9])
+        ent_input_name_code_meli.insert(0,f"0{i[9]}")
         ent_input_name_code_meli.grid(row=0,column=0)
         lbl_input_name_tel_sabet = CTkLabel(frame_down,text=" : تلفن ثابت",font=font)
         lbl_input_name_tel_sabet.grid(row=1,column=5)
@@ -1342,8 +1485,8 @@ def login():
     
 # login()
 # paziresh_()
-tarikh_()
-# dashbord()
+# tarikh_()
+dashbord()
 # show(1)
 
 root.mainloop()
